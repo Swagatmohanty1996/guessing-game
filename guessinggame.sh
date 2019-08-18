1d9cd577e44fb69eccd7df6beaab722835a0a097
swagatmohanty1996@gmail.com <a625533@fmr.com>
2019-08-18 19:22:34 +0530

    commit

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "[Welcome to Guessing game]"
+echo "[Welcome to Guessing game v0.1]"
 
 function ask {
 	echo "Please enter the number of files in the current directory:"
